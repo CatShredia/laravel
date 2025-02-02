23fcda11648f5bdd3f4dc8f6e72eedd06e12dfb3
CatShredia <sasha990g@gmail.com>
2025-02-03 01:42:30 +0300

    laravel add-venicle v0.3.4.1

---
+++ laravel-documentation/app/Http/Controllers/VenicleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Venicle;
+use Illuminate\Http\Request;
+
+class VenicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Venicle $venicle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Venicle $venicle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Venicle $venicle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Venicle $venicle)
+    {
+        //
+    }
+}
