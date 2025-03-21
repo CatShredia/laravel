85e9bf34d0cabb0a45d055d63cf9eeceff725f10
CatShredia <sasha990g@gmail.com>
2025-03-21 10:54:00 +0300

    step 0.1 styles template

--- project/resources/views/posts/index.blade.php
+++ project/resources/views/posts/index.blade.php
@@ -1,1 +1,5 @@
-Hello Blog!
+@extends('layouts.default')
+
+@section('content')
+
+@endsection
--- project/vite.config.js
+++ project/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                "resources/sass/app.scss",
+                "resources/assets/style.css",
+                "resources/assets/style.css.map",
+                "resources/js/app.js",
+                "resources/js/counter.js",
+                "resources/js/loader.js",
+                "resources/js/main.js",
+                "resources/js/timer.js",
             ],
             refresh: true,
         }),
