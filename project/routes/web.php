aae4d16d4cf0dd178f695d0bb2b6cdb276ce9479
CatShredia <sasha990g@gmail.com>
2025-03-25 14:39:41 +0300

    jquery bootstrap init

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/test', [TestController::class, 'index']);
