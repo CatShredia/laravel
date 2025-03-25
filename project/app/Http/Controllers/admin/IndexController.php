5ff0c6c6c9850854a5860c5369b785893ef07dcc
CatShredia <sasha990g@gmail.com>
2025-03-25 15:44:32 +0300

    admin panel init

---
+++ project/app/Http/Controllers/admin/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return view('admin.index');
+    }
+
+    public function redirect()
+    {
+        return redirect(route('admin.index'));
+    }
+}
--- project/routes/web.php
+++ project/routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\admin\IndexController as AdminIndexController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/test', [TestController::class, 'index']);
+Route::get('/test', [TestController::class, 'index'])->name('test');
+
+Route::group(['namespace' => 'admin', 'prefix' => 'admin'], function () {
+    Route::get('/', [AdminIndexController::class, 'redirect']);
+    Route::get('/index', [AdminIndexController::class, 'index'])->name('admin.index');
+});
